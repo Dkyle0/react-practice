0f0b41c03c3e14be591e5fa10557977184dd784d
Dkyle0 <50553897+Dkyle0@users.noreply.github.com>
2024-02-09 16:47:25 +0300

    bff++

--- src/bff/bff.js
+++ src/bff/bff.js
@@ -1,3 +1,9 @@
+const generateDate = () =>
+	new Date(Math.random() * 1000000000000 + 1999999999999)
+		.toISOString()
+		.substring(0, 16)
+		.replace('T', ' ');
+
 export const server = {
 	async authorize(authLogin, authPassword) {
 		const users = await fetch('http://localhost:3005/users').then((loadedUsers) =>
@@ -18,6 +24,7 @@ export const server = {
 				res: null,
 			};
 		}
+
 		const session = {
 			error: null,
 			res: {
@@ -33,6 +40,58 @@ export const server = {
 				},
 			},
 		};
-		return session;
+
+		return {
+			error: null,
+			res: session,
+		};
+	},
+	async register(regLogin, regPassword) {
+		const users = await fetch('http://localhost:3005/users').then((loadedUsers) =>
+			loadedUsers.json(),
+		);
+
+		const user = users.find(({ login }) => login === regLogin);
+		if (user) {
+			return {
+				error: 'Такой логин уже занят',
+				res: null,
+			};
+		}
+
+		await fetch('http://localhost:3005/users', {
+			method: 'POST',
+			headers: {
+				'Conent-Type': 'application/json;charset=utf-8',
+			},
+			body: JSON.stringify({
+				id: '01',
+				login: regLogin,
+				password: regPassword,
+				registredAt: generateDate(),
+				role_id: 2,
+			}),
+		});
+
+		const session = {
+			error: null,
+			res: {
+				logout() {
+					Object.keys(
+						session.array.forEach((key) => {
+							delete session[key];
+						}),
+					);
+				},
+				removeComment() {
+					console.log('удаление комментария');
+				},
+			},
+		};
+
+		return {
+			error: null,
+			res: session,
+		};
 	},
 };
